e3d1db1186a793c3b1c6eaa1416fc94d23650c15
Emmanuel Omonzebaguan <emmizy2015@gmail.com>
2020-08-10 04:16:20 +0100

    working on the post on the field section of the twitter clone app

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './TweetBox.css';
+import { Avatar, Button } from '@material-ui/core';
 
 function TweetBox() {
     return (
-        <div>
-            
+        <div className="tweetBox">
+            <form>
+                <div className="tweetBox__input">
+                    <Avatar src=""/>
+                    <input type="text" placeholder="What's Happening?" name="tweet"/>
+                </div>
+
+                <input type="text" placeholder="Optional: Enter Image URL" className="tweetBox__imageInput"/>
+                <Button className="tweetBox__tweetButton">Tweet</Button>
+            </form>     
         </div>
     )
 }
