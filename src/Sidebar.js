b52e3f674308c21c6179fc8a10049b53ead47d17
Emmanuel Omonzebaguan <emmizy2015@gmail.com>
2020-08-10 01:45:33 +0100

    using the material ui for fonts and the main team

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import "./Sidebar.css";
 import TwitterIcon from '@material-ui/icons/Twitter';
+import SidebarOption from "./SidebarOption";
+import HomeIcon from '@material-ui/icons/Home';
+import SearchIcon from '@material-ui/icons/Search';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 function Sidebar() {
     return (
@@ -9,12 +18,17 @@ function Sidebar() {
             <TwitterIcon/>
 
             {/* sidebarOption */}
-            {/* sidebarOption */}
-            {/* sidebarOption */}
-            {/* sidebarOption */}
-            {/* sidebarOption */}
+            <SidebarOption Icon={HomeIcon} text="Home"/>
+            <SidebarOption Icon={SearchIcon} text="Explore"/>
+            <SidebarOption Icon={NotificationsNoneIcon} text="Notification"/>
+            <SidebarOption Icon={MailOutlineIcon} text="Messages"/>
+            <SidebarOption Icon={BookmarkBorderIcon} text="Bookmarks"/>
+            <SidebarOption Icon={ListAltIcon} text="List"/>
+            <SidebarOption Icon={PermIdentityIcon} text="Profile"/>
+            <SidebarOption Icon={MoreHorizIcon} text="More"/>
 
             {/* Twitt button */}
+            
         </div>
     )
 }
--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./SidebarOption.css";
 
-function SidebarOption() {
+
+function SidebarOption({text, Icon}) {
     return (
-        <div>
-            
+        <div className="sidebarOption">
+            <Icon />
+            <h2>{text}</h2>
         </div>
     )
 }
