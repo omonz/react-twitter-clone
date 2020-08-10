51b36600074943dd68cb898fe8e1b00eef798fba
Emmanuel Omonzebaguan <emmizy2015@gmail.com>
2020-08-10 01:01:47 +0100

    started building the twitter clone app along with clever programmer

---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Sidebar.css";
+import TwitterIcon from '@material-ui/icons/Twitter';
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            {/* Twitter Icon */}
+            <TwitterIcon/>
+
+            {/* sidebarOption */}
+            {/* sidebarOption */}
+            {/* sidebarOption */}
+            {/* sidebarOption */}
+            {/* sidebarOption */}
+
+            {/* Twitt button */}
+        </div>
+    )
+}
+
+export default Sidebar
