bccabc63e6cf33d9a031d7659e3fc00c5e74a776
Emmanuel Omonzebaguan <emmizy2015@gmail.com>
2020-08-12 00:20:17 +0100

    connected to firebase and about to deploy the react twitter clone to firebase

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import FlipMove from 'react-flip-move';
 import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post';
@@ -8,9 +9,9 @@ function Feed() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        db.collection('posts').onSnapshot(snapshot => (
-            setPosts(snapshot.docs.map(doc => doc.data()))
-    ))
+        db.collection("posts").onSnapshot((snapshot) =>
+          setPosts(snapshot.docs.map((doc) => doc.data()))
+        );
     }, []);
     return (
       <div className="feed">
@@ -18,19 +19,21 @@ function Feed() {
           <h2>Home</h2>
         </div>
         <TweetBox />
-        {posts.map(post => (
-
-        <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          image={post.image}
-          avatar={post.avatar}
-        />
-         ))}
+        <FlipMove>
+          {posts.map((post) => (
+            <Post
+            key={post.text}
+              displayName={post.displayName}
+              username={post.username}
+              verified={post.verified}
+              text={post.text}
+              image={post.image}
+              avatar={post.avatar}
+            />
+          ))}
+        </FlipMove>
       </div>
     );
 }
 
-export default Feed
+export default Feed;
--- src/components/feed/Post.js
+++ src/components/feed/Post.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import './Post.css';
 import { Avatar } from '@material-ui/core';
 import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
@@ -7,9 +7,10 @@ import RepeatIcon from '@material-ui/icons/Repeat';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import PublishIcon from '@material-ui/icons/Publish';
 
-function Post({displayName, username, verified, text, image, avatar}) {
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
     return (
-      <div className="post">
+      <div className="post" ref={ref}>
         <div className="post__avatar">
           <Avatar src={avatar} />
         </div>
@@ -38,6 +39,7 @@ function Post({displayName, username, verified, text, image, avatar}) {
         </div>
       </div>
     );
-}
+  }
+);
 
 export default Post 
